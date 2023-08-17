a69f214f5cf403a2128395628a33c0421e027c79
kunal1234523 <kunal.joshi@myself.com>
2023-08-17 08:05:47 +0530

    Empty commit

--- index.js
+++ index.js
@@ -1,17 +1,30 @@
+function ocrImage(url, i) {
+    Tesseract.recognize(
+    url,
+    'eng',
+    { logger: m => console.log(m) }
+  ).then(({ data: { text } }) => {
+    document.getElementById(`ocr-output-${i}`).innerText = text;
+  })
+}
+
 function NewTab(url) {
     window.open(url, "_blank");
 }
 
 imageElement = document.querySelector(".js-images")
 
-let image_string_html = ""
+let total_html = ""
 
 for (let i = 0; i < 50 ; i++) {
-    image_url = `https://pablo1234523.github.io/placement-help/screenshots/s${i}.jpeg`
+    let image_url = `https://pablo1234523.github.io/placement-help/screenshots/s${i}.jpeg`
     console.log(image_url)
-    q_no_html = `<p class='css-qno'>${i+1}.</p>`
-    image_string_html += q_no_html + `<img src='screenshots/s${i}.jpeg' class='css-images' onclick=NewTab('${image_url}')> <hr>`
+    let q_no_html = `<p class='css-qno'>${i+1}.</p>`
+    let image_string_html = `<img src='screenshots/s${i}.jpeg' class='css-images' onclick=NewTab('${image_url}')>`;
+    let ocr_button_html = `<button class='ocr-button' onclick=ocrImage('${image_url}',${i})>OCR</button>`;
+    let ocr_output_html = `<div class='ocr-output-class' id='ocr-output-${i}'></div>`;
     
+    total_html += q_no_html + image_string_html + ocr_button_html + ocr_output_html + '<hr>'
 }
 
-imageElement.innerHTML = `<div>${image_string_html}</div>`
+imageElement.innerHTML = `<div>${total_html}</div>`
