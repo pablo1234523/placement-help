63df25648385104b30f2472d074cd3d3eee735d2
kunal1234523 <kunal.joshi@myself.com>
2023-08-17 05:38:25 +0530

    Empty commit

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
-imageElement = document.querySelector(".js-images")
-let image_string_html = ""
-for (let i = 0; i < 50 ; i++) {
-    image_string_html += `<img src='screenshots/s${i}.jpeg' class = 'js-images css-images'>`
+function NewTab(url) {
+    window.open(url, "_blank");
 }
-console.log(image_string_html)
+
+imageElement = document.querySelector(".js-images")
+
+let image_string_html = ""
+
+for (let i = 0; i < 50 ; i++) {
+    image_url = `https://pablo1234523.github.io/placement-help/screenshots/s${i}.jpeg`
+    console.log(image_url)
+    image_string_html += `<img src='screenshots/s${i}.jpeg' class='css-images' onclick=NewTab('${image_url}')>`
+    
+}
+
 imageElement.innerHTML = image_string_html
