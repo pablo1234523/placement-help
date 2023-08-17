a90d56e595daa4b326719c7f30ae3cbf827e043f
kunal1234523 <kunal.joshi@myself.com>
2023-08-17 06:44:53 +0530

    Empty commit

--- index.js
+++ index.js
@@ -9,8 +9,9 @@ let image_string_html = ""
 for (let i = 0; i < 50 ; i++) {
     image_url = `https://pablo1234523.github.io/placement-help/screenshots/s${i}.jpeg`
     console.log(image_url)
-    image_string_html += `<img src='screenshots/s${i}.jpeg' class='css-images' onclick=NewTab('${image_url}')>`
+    q_no_html = `<p class='css-qno'>${i+1}.</p>`
+    image_string_html += q_no_html + `<img src='screenshots/s${i}.jpeg' class='css-images' onclick=NewTab('${image_url}')> <hr>`
     
 }
 
-imageElement.innerHTML = image_string_html
+imageElement.innerHTML = `<div>${image_string_html}</div>`
